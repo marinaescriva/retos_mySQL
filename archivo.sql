c31f8d8c010f3eb65effdeff0d0a7b21c1a5b6d5
Marina <marinaescriva.24@gmail.com>
2024-02-21 13:25:05 +0100

    my first MySQL

---
+++ archivo.sql
@@ -0,0 +1,18 @@
+
+/*RETO 0
+
+
+/* RETO1
+ Aplicación de Gestión de Tareas:
+Imagina que estás construyendo una aplicación para gestionar tareas. Los usuarios pueden
+crear listas de tareas, agregar tareas a esas listas, establecer fechas límite y marcar las
+tareas como completadas. ¿Cuáles serían las entidades principales?
+*/
+
+CREATE DATABASE reto_1
+
+USE reto_1
+
+
+
+
